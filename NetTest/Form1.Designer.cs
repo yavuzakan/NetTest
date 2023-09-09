1d9598ea10aeeb00afba9b8d3f5796f8cf4e162e
yavuz akan <yavuz.akan@gmail.com>
2023-09-09 12:20:17 +0300

    Proje dosyası ekle.

---
+++ NetTest/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace NetTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "www.google.com";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 38);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(256, 322);
+            this.textBox2.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 367);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ NetTest/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetTest
+{
+    public partial class Form1 : Form
+    {
+        public static string newline = Environment.NewLine;
+        public Form1()
+        {
+            InitializeComponent();
+            ping();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ping();
+        }
+
+        public void ping()
+        {
+            button1.Enabled = false;
+            Ping ping = new Ping();
+            PingReply cevap = ping.Send(textBox1.Text);
+
+            if (cevap.Status == IPStatus.Success)
+            {
+                //  textBox3.Text += gelenip + newline;
+                // string time = DateTime.Now.ToString("h:mm:ss tt");
+                // textBox2.Text +=  "tamam" + newline;
+
+
+            }
+            else
+            {
+
+                string time = DateTime.Now.ToString(" dd/MM/yyyy h:mm:ss tt");
+                textBox2.Text += "Hatalı "+ time + newline;
+            }
+
+
+
+
+            button1.Enabled = true;
+       
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ping();
+        }
+    }
+}
